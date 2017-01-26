100454587344c7dd97ebedcbfa07d84c91aa433c
Bencsó <gabor.bencsik13@gmail.com>
2017-01-26 17:09:01 +0100

    refactored to be modular

--- emojiSelector.js
+++ emojiSelector.js
@@ -1,25 +1,26 @@
-// const data = require('./data.json');
+const data = require('/Users/bencso/Desktop/data.json');
 const emojiList = [":joy:", ":clown-face:", ":yo:"];
 const emojiScore = {
   ":joy:": 1
 };
 
-let emojis = {
-  ":joy:": 1
-};
-
-function findEmoji(data) {
-  let textArray = data.text.split(' ');
-  textArray.forEach( function(item) {
+const selector = (function () {
+  let emojis = [];
+  let findEmoji = function(data) {
+    let textArray = data.text.split(' ');
+    textArray.forEach( function(item) {
       if (emojiList.includes(item)) {
-        console.log(item);
-        if (Object.keys(emojis).includes(item)) {
-          emojis[item]+=1;
-        } else {
-          emojis[item] = 1;
-        }
-      }
+        emojis.push(item);
+      };
     });
-});
+    return emojis;
+  };
 
-console.log(emojis);
+  return {
+    findEmoji: findEmoji
+  };
+
+})();
+
+// selector.findEmoji(data);
+console.log(selector.findEmoji(data));
