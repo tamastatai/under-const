3ee58e770aef35193493036e7b81d654512dbcd3
Bencsó <gabor.bencsik13@gmail.com>
2017-01-27 16:01:00 +0100

    server

--- server/server.js
+++ server/server.js
@@ -1,43 +1,66 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-let data = {};
+const mysql = require('mysql');
 const cors = require('cors');
 const IncomingWebhook = require('@slack/client').IncomingWebhook;
+const url = 'https://slack.com/api/auth.test';
+const selector = require('./selector.js');
+
 let token = process.env.SLACK_API_TOKEN || '';
-const slackEvents = require('slack-events-listener')(token, onSlackEvent);
-
-function onSlackEvent(event, cb) {
-  // do something. call cb with err if you want Slack to resend the message (your database might be down)
-  console.log('tes');
-
-}
+let data = require('./data.json');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors());
 
-app.get('/', function siteIsRunning(req,res) {
-
-  res.send('Server is running').status(200);
+const connection = mysql.createConnection({
+  // refaktor for proc.env
+  host: 'sql11.freesqldatabase.com',
+  user: 'sql11155834',
+  password: 'FlPZB53K7G',
+  database: 'sql11155834',
 });
 
-app.use('/slack_events', bodyParser.json(), slackEvents);
-
-app.get('/raw-data', function siteIsRunning(req,res) {
-
-  res.send(data).status(200);
+connection.connect(function connectMsql(error) {
+  if (error) {
+    console.log('BAMMM ERRRRRRRRROR', error);
+  } else {
+    console.log('It Works, NICE!');
+  }
 });
 
-app.post('/webhook', function getData(req,res) {
-  data = req.body;
-
-  console.log(data);
-
-  res.send().status(200);
+app.get('/messages', (req, res) => {
+ let time = new Date();
+ connection.query({
+    sql: "SELECT text_length, channel_name, emojis FROM sql11155834.underconst WHERE timestamp ORDER BY timestamp DESC LIMIT 15;",
+    // values: [time]
+  }, function callBack(err, rows) {
+      if (err) {
+        console.log(err.toString());
+        return;
+      }
+      res.send(rows);
+    });
 });
 
+app.post('/slack-events', function getData(req,res) {
+ data = req.body;
+ let info = selector.sendData(data);
+ console.log(info);
+
+ connection.query({
+    sql: "INSERT INTO sql11155834.underconst (text_length, channel_name, emojis) VALUES (?, ?, ? );",
+    values: [info.text_length, info.channel_name, info.emojis]
+  }, function callBack(err, rows) {
+      if (err) {
+        console.log(err.toString());
+        return;
+      }
+      res.send(rows);
+    });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
